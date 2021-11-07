8a4ce10bdd549f55f9ea29a41595945691fbd26e
Linh Hoang <ellehoang@hotmail.com>
2021-11-07 03:10:13 -0500

    implement fetchCoordsByIP func with error/statusCode/Latitude&Longitude callback, and implement export module to index.js

--- iss.js
+++ iss.js
@@ -8,7 +8,6 @@ const request = require('request');
  *   - An error, if any (nullable)
  *   - The IP address as a string (null if error). Example: "162.245.144.188"
  */
-
 const fetchMyIP = function(callback) {
   // use request to fetch IP address from JSON API
   request(`https://api.ipify.org?format=json`, (error, response, body) => {
@@ -36,4 +35,31 @@ const fetchMyIP = function(callback) {
   });
 };
 
-module.exports = { fetchMyIP };
+/**
+ * Makes a single API request to retrieve the lat/lng for a given IPv4 address.
+ * Input:
+ *   - The ip (ipv4) address (string)
+ *   - A callback (to pass back an error or the lat/lng object)
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The lat and lng as an object (null if error). Example:
+ *     { latitude: '49.27670', longitude: '-123.13000' }
+ */
+const fetchCoordsByIP = function(ip, callback) {
+  request(`http://ip-api.com/json/${ip}`, (error, response, body) => {
+    if (error) return callback(error, null);
+
+    if (response.statusCode !== 200) {
+      callback(Error(`Status Code ${response.statusCode} when fetching Coordinates for IP: ${body}`), null);
+      return;
+    }
+
+    const { lat, lon } = JSON.parse(body);
+    callback(null, { lat, lon});
+  })
+}
+
+module.exports = {
+  //fetchMyIP
+  fetchCoordsByIP
+};
