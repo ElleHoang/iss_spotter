98ec5403028cd39bcaa430147180f53ac4c1d9ca
Linh Hoang <ellehoang@hotmail.com>
2021-11-07 09:22:22 -0500

    implement fetchISSFlyOverTimes import module and mocha testing for error searching for latitude and longitude of a given IP address

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 //const { fetchMyIP } = require('./iss');
-const { fetchCoordsByIP } = require('./iss');
+//const { fetchCoordsByIP } = require('./iss');
+const { fetchISSFlyOverTimes } = require('./iss');
 /*
 fetchMyIP((error, ip) => {
   if(error) {
@@ -8,7 +9,7 @@ fetchMyIP((error, ip) => {
   }
   console.log("It worked! Return IP: ", ip);
 });
-*/
+
 fetchCoordsByIP('142.186.103.182', (error, coordinates) => {
   if (error) {
     console.log("It didn't work!", error);
@@ -17,3 +18,14 @@ fetchCoordsByIP('142.186.103.182', (error, coordinates) => {
 
   console.log('It worked! Returned coordinates: ', coordinates);
 });
+
+const coords = { latitude: '43.7063', longitude: '-79.4202' };
+fetchISSFlyOverTimes(coords, (error, passTimes) => {
+  if (error) {
+    console.log("It didn't work! ", error);
+    return;
+  }
+
+  console.log('It worked! Returned flyover times: ', passTimes);
+});
+*/
