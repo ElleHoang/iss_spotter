cfe229e8a770105bcef20f2e12ae3ff163f014e6
Linh Hoang <ellehoang@hotmail.com>
2021-11-07 03:12:25 -0500

    implement fetchCoordsByIP import module and mocha testing for error searching for coordinates from IP

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
+//const { fetchMyIP } = require('./iss');
+const { fetchCoordsByIP } = require('./iss');
 /*
-const { fetchMyIP } = require('./iss');
-
 fetchMyIP((error, ip) => {
   if(error) {
     console.log("It didn't work!", error);
@@ -9,3 +9,11 @@ fetchMyIP((error, ip) => {
   console.log("It worked! Return IP: ", ip);
 });
 */
+fetchCoordsByIP('142.186.103.182', (error, coordinates) => {
+  if (error) {
+    console.log("It didn't work!", error);
+    return;
+  }
+
+  console.log('It worked! Returned coordinates: ', coordinates);
+});
